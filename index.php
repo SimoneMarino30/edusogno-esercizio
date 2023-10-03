8745ef29a194b7ddcc2685b4980ad41b977c3581
Simone Marino <simo.marino@outlook.it>
2023-10-03 09:33:37 +0200

    init

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edusogno</title>
+    <!-- CSS LINK -->
+    <link rel="stylesheet" href="./assets/styles/style.css" />
 </head>
 
 <body>
-
+    <h1>Hello Edusogno!</h1>
 </body>
 
 </html>
