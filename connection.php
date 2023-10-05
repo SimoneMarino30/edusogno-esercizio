59d7e6a12fdd86c0f3bfb91666ab816f4737fc71
Simone Marino <simo.marino@outlook.it>
2023-10-05 23:40:32 +0200

    code formatting

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // * PREPARO I PARAMETRI DI CONNESSIONE
 const DB_SERVERNAME = "localhost";
@@ -11,16 +11,15 @@ const DB_NAME = "edusogno_db";
 try {
   $conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
   echo "Connessione ok";
-} catch(Exception $e) {
+} catch (Exception $e) {
   // * SE INCONTRO UN ERRORE 
-  
+
   // * PREPARO VARIABILE ERRORE DA STAMPARE
   $error = "Connessione fallita:<br>" . $e->getMessage();
 
   // * INCLUDO UNA PAGINA DI ERRORE CUSTOMIZZATA
   // include(__DIR__ . "/pages/error_page.php");
-  
+
   // * INTERROMPO L'ESECUZIONE DEL CODICE
   exit;
 }
-?>
--- partials/nav.php
+++ partials/nav.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Edusogno</title>
-    <!-- GOOGLE FONTS LINK -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,200;9..40,400;9..40,700&family=Space+Grotesk&display=swap"
-      rel="stylesheet"
-    />
-    <!-- CSS LINK -->
-    <link rel="stylesheet" href="./assets/styles/style.css" />
-  </head>
-  <body>
-    <div class="topnav">
-      <!-- <a href="https://edusogno.com/"> -->
-        <!-- redirect login page INDEX -->
-      <a href="./">
-        <img src="https://edusogno.com/logo-black.svg" alt="edusogno-logo" />
-      </a>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Edusogno</title>
+  <!-- GOOGLE FONTS LINK -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,200;9..40,400;9..40,700&family=Space+Grotesk&display=swap"
+    rel="stylesheet" />
+  <!-- CSS LINK -->
+  <link rel="stylesheet" href="./assets/styles/style.css" />
+</head>
+
+<body>
+
+  <div class="topnav">
+    <!-- <a href="https://edusogno.com/"> -->
+    <!-- redirect login page INDEX -->
+    <a href="./">
+      <img src="https://edusogno.com/logo-black.svg" alt="edusogno-logo" />
+    </a>
   </div>
   <main class="container">
